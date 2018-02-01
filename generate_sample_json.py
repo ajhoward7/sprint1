aa9b0ee28a6774ebe0b60bbd9ee6f6f1ef855a32
Tim Lee <tim.david.lee@gmail.com>
2018-02-01 11:24:09 -0800

    generated 1000 sample jsons

--- generate_sample_json.py
+++ generate_sample_json.py
@@ -10,5 +10,5 @@ json_list = []
 for i in range(100):
     json_list.append({"name": rn.random_nicks()[0], "prop": { "age": np.random.randint(20, 150), "zipcode": np.random.randint(90000, 99999), "DMID" : np.random.randint(0, 999999) }})
 
- for json in json_list:
-    pprint(json)
+for json in json_list:
+ 	pprint(json)
