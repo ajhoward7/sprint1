71a5674c5950dcc155a93926777d0fc3e219bb8f
Tim Lee <tim.david.lee@gmail.com>
2018-02-03 22:28:24 -0800

    fixed utf-8 encoding issue on sample data

--- generate_sample_json.py
+++ generate_sample_json.py
@@ -8,7 +8,7 @@ rn.random_nicks()
 
 json_list = []
 for i in range(100):
-    json_list.append({"name": rn.random_nicks()[0], "prop": { "age": np.random.randint(20, 150), "zipcode": np.random.randint(90000, 99999), "DMID" : np.random.randint(0, 999999) }})
+    json_list.append({"name": rn.random_nicks()[0].encode('utf-8'), "prop": { "age": np.random.randint(20, 150), "zipcode": np.random.randint(90000, 99999), "DMID" : np.random.randint(0, 999999) }})
 
 for json in json_list:
  	print str(json).replace("'",'"')
