e84bc3d012722e7443724fc4a49aeb5e486fa4f3
Tim Lee <tim.david.lee@gmail.com>
2018-02-03 23:10:54 -0800

    tim's version of json digest with status messages

--- json_digest.py
+++ json_digest.py
@@ -56,7 +56,7 @@ def write_statsfile(stat_strs, prefix):
         f.write('\n'.join(stat_strs))
 
 
-parser = argparse.ArgumentParse(description = 'json ingestion library')
+parser = argparse.ArgumentParser(description = 'json ingestion library')
 parser.add_argument('--prefix', help = 'file prefix that we will be searching for ')
 args = parser.parse_args()
 
