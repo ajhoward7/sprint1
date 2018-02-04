41d0f38f9c5dafeb4155b92316b6c8c997e8fd7e
Tim Lee <tim.david.lee@gmail.com>
2018-02-03 22:22:45 -0800

    fixed quoting issue in json encoding

--- generate_sample_json.py
+++ generate_sample_json.py
@@ -11,4 +11,4 @@ for i in range(100):
     json_list.append({"name": rn.random_nicks()[0], "prop": { "age": np.random.randint(20, 150), "zipcode": np.random.randint(90000, 99999), "DMID" : np.random.randint(0, 999999) }})
 
 for json in json_list:
- 	pprint(json)
+ 	print str(json).replace("'",'"')
