f8ca7e18e46548fcdefc927da373db6289549418
Tim Lee <tim.david.lee@gmail.com>
2018-02-12 20:47:32 -0800

    retrofitted script for testtest account

--- deploy_standalone.py
+++ deploy_standalone.py
@@ -1,14 +1,9 @@
-# ===============================================================
-# Team select stars
-# 
-# Tim Lee
-# Jade Yun
-# Patrick Yang
-# Alex Howard
-# ===============================================================
-
 import paramiko
 import sys
+from os.path import expanduser
+import time
+import json
+import argparse
 
 def connect(key_url, server_url, username='testtest'):
     """
@@ -22,6 +17,14 @@ def connect(key_url, server_url, username='testtest'):
     print "connected to %s " % server_url
     c.connect(hostname = server, username = username, pkey = k)
     return c
+
+
+def get_timestamp():
+    """
+    Simply returns a timestamp, used for logging
+    """
+    return time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
+
 
 def git_pull(client):
     """
@@ -48,7 +51,7 @@ def update_crontab(client, prefix):
     msg = '* * * * * python /home/ec2-user/sprint1/json_digest.py'
     try:
         client.exec_command('crontab -r')
-        client.exec_command('(crontab -l 2>/dev/null; echo "*/5 * * * * python /home/testtest/sprint1/json_digest.py --prefix %s") | crontab -' % prefix)
+        client.exec_command('(crontab -l 2>/dev/null; echo "*/5 * * * * python /home/ec2-user/sprint1/json_digest.py --prefix %s") | crontab -' % prefix)
         print 'cron tab updated'
     except Exception as e:
         print(e)        
@@ -58,6 +61,40 @@ def deploy(key_url, server_url, prefix):
     c = connect(key_url, server_url)
     git_pull(c)
     update_crontab(c, prefix)
-    print "script successfully deployed"
+    print "Script deployed successfully"
     c.close()
 
+
+parser = argparse.ArgumentParser(description = 'json ingestion library')
+parser.add_argument('-k', help = 'location of the server pem key')
+parser.add_argument('-s', help = 'server we are connecting to')
+parser.add_argument('-p', help = 'file prefix that we will be searching for ')
+args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    valid = True
+    msg = []
+
+    if args.k is not None:
+        key_url = args.k
+    else:
+        valid = False
+        msg.append("please provide location of key with '-k'")
+
+    if args.s is not None:
+        server_url = args.s
+    else:
+        valid = False
+        msg.append("please provide service host with '-s'")
+
+    if args.p is not None:
+        prefix = args.p
+    else:
+        valid = False
+        msg.append("please file prefix to search '-p'")
+    
+    if valid:
+        deploy(key_url, server_url, prefix)
+    else:
+        print '\n'.join(msg)
