b39197ec9c863fc6d7a09b6eda8b1e7094d0e569
Tim Lee <tim.david.lee@gmail.com>
2018-02-19 13:26:37 -0800

    removing test scripts

--- run_gunicorn.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gunicorn -D --threads 4 -b 0.0.0.0:8080 --access-logfile server.log --timeout 60 server:app
--- stop_gunicorn.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kill -9 `ps aux |grep gunicorn |grep your_app_name | awk '{ print $2 }'`  # will kill all of the workers
