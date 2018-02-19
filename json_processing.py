2165c0a0de746b47b5c31dad82564271136776b7
Tim Lee <tim.david.lee@gmail.com>
2018-02-18 18:49:53 -0800

    changed the json library to be more generic

--- json_processing.py
+++ json_processing.py
@@ -1,17 +1,64 @@
 import json
 import os
 
-
 def valid_json(json_str):
-	if json_str == "":
+	"""
+	json validation function. This function takes in json strings
+	and checks the following:
+
+	- If the string is blank
+	- if the string is empty
+	- if the string can be converted (missing {})
+	- Check to make sure that the 'Prop' value in data
+	- check to make sure 'age' exists and
+		- age >= 0
+		- age is an int
+	- check to make sure that the name exists
+	"""
+	try:
+
+		# nothing passed
+		if json_str is None:
+			return False
+
+		# blank strings
+		if json_str == "":
+			return False
+
+		# convert to json
+		json_v = json.loads(line)
+
+		# check 'prop field'
+		props = json_v.get('prop')
+		if props  is None:
+			return False
+
+		# check age field
+		age = props.get('age')
+		if age is None:
+			return False
+
+		# check proper age formatting
+		if type(age) != int:
+			return False
+
+		if age < 0:
+			return False
+
+		# checks for name fields
+		name = props.get('name')
+		if name is None:
+			return False
+
+		# if all conditions are passed
+		return True
+
+	except Exception as e:
 		return False
+		print(e)
 
-	json_v = json.loads(line)
 
-	if json_v.get('prop') is None:
-		return 
-
-def extract_ages(json_strs):
+def extract_fields(json_strs):
     """
     takes in an array of json strings and converts them to
     actual json. Some notes:
@@ -22,20 +69,20 @@ def extract_ages(json_strs):
     unicode, otherwise the python json library will also
     have a similiarly difficult time with it
     """
-    return_stats = []
-    for line in json_strs:
-        if line != "":
-            try:
-                json_v = json.loads(line)
-                props = json_v.get('prop')
-                if props is not None:
-                    age = props.get('age')
-                    name = json_v.get('name')
-                    if (type(age) == int) and (name is not None):
-                        if age >= 0:
-                            return_stats.append("%s\t%d" % (str(name), age))
-                    
-            except Exception as e:
-                print(e)
-    print "%d valid jsons found" % len(return_stats)
-    return return_stats
+    names_and_ages = []
+
+    # goes through input strings
+    for json_item in json_strs:
+
+    	# checks if valid
+    	if valid_json(json_item):
+    		json_v = json.loads(line)
+
+    		row = []
+    		for field in ['age','prop']
+    			row.append(json_v[field])
+
+    		names_and_ages.append(row)
+
+    print "%d valid jsons found" % len(names_and_ages)
+    return names_and_ages
