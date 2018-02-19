962b0c880b4213ba20d2cb5de4e3f652acdcfee2
Alex Howard <Alex@Alexs-MacBook-Pro.local>
2018-02-18 20:15:59 -0800

    Pushing server.py file

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def parse_request():
+    data = request.json
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.INFO)
+    handler = TimedRotatingFileHandler("test.log",
+                                       when="m",
+                                       interval=2,
+                                       backupCount=5)
+    logger.addHandler(handler)
+
+    logger.info(data)
+
+    return "Here, have some HTML in return\n"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
