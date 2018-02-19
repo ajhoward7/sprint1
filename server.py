cd34ea417619280d8825470e9af6f85951457350
Alex Howard <Alex@Alexs-MacBook-Pro.local>
2018-02-18 20:24:51 -0800

    Tweaks

--- server.py
+++ server.py
@@ -5,20 +5,22 @@ from logging.handlers import TimedRotatingFileHandler
 
 app = Flask(__name__)
 
+# Setting up the logger:
+logger = logging.getLogger("Rotating Log")
+logger.setLevel(logging.INFO)
+handler = TimedRotatingFileHandler("test.log",
+                                   when="m",
+                                   interval=2,
+                                   backupCount=5)
+logger.addHandler(handler)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def parse_request():
-    data = request.json
+    data = request.json  # Take JSON data from HTTP
     data = " ".join(data.split("\n"))  # remove hard returns
 
-    logger = logging.getLogger("Rotating Log")
-    logger.setLevel(logging.INFO)
-    handler = TimedRotatingFileHandler("test.log",
-                                       when="m",
-                                       interval=2,
-                                       backupCount=5)
-    logger.addHandler(handler)
-
-    logger.info(data)
+    logger.info(data)  # log
 
     return "Here, have some HTML in return\n"
 
